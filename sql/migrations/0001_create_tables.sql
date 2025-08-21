cce26fbe79a7ffb8fb073376cc4d86fb796be899
AlphaByte02 <47041609+AlphaByte02@users.noreply.github.com>
2025-08-21 17:56:59 +0200

    Add update_modified_column function and triggers

--- sql/migrations/0001_create_tables.sql
+++ sql/migrations/0001_create_tables.sql
@@ -1,35 +1,53 @@
 -- Postgres schema for FairSplit
+CREATE OR REPLACE FUNCTION update_modified_column () RETURNS TRIGGER AS $$
+BEGIN
+NEW.updated_at = now();
+RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
 CREATE TABLE users (
-  id UUID PRIMARY KEY,
-  email TEXT NOT NULL UNIQUE,
-  username TEXT UNIQUE,
-  picture TEXT,
-  paypal_username TEXT,
-  iban TEXT,
-  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
-  updated_at TIMESTAMPTZ DEFAULT now() NOT NULL
+    id UUID PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    username TEXT UNIQUE,
+    picture TEXT,
+    paypal_username TEXT,
+    iban TEXT,
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT now() NOT NULL
 );
+
+
+CREATE TRIGGER update_users_updated_at BEFORE
+UPDATE ON users FOR EACH ROW
+EXECUTE PROCEDURE update_modified_column ();
 
 
 CREATE TABLE sessions (
-  id UUID PRIMARY KEY,
-  created_by_id UUID NOT NULL REFERENCES users (id),
-  name TEXT NOT NULL,
-  is_closed BOOLEAN NOT NULL DEFAULT FALSE,
-  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
-  updated_at TIMESTAMPTZ DEFAULT now() NOT NULL,
-  UNIQUE (created_by_id, name)
+    id UUID PRIMARY KEY,
+    created_by_id UUID NOT NULL REFERENCES users (id),
+    name TEXT NOT NULL,
+    is_closed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    UNIQUE (created_by_id, name)
 );
+
+
+CREATE TRIGGER update_sessions_updated_at BEFORE
+UPDATE ON sessions FOR EACH ROW
+EXECUTE PROCEDURE update_modified_column ();
 
 
 CREATE INDEX idx_session_created_by_id ON sessions (created_by_id);
 
 
 CREATE TABLE session_participants (
-  session_id UUID NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
-  user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
-  PRIMARY KEY (session_id, user_id)
+    session_id UUID NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    PRIMARY KEY (session_id, user_id)
 );
 
 
@@ -40,15 +58,20 @@ CREATE INDEX idx_session_participants_user_id ON session_participants (user_id);
 
 
 CREATE TABLE transactions (
-  id UUID PRIMARY KEY,
-  session_id UUID NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
-  payer_id UUID NOT NULL REFERENCES users (id),
-  amount NUMERIC(12, 2) NOT NULL CHECK (amount >= 0),
-  description TEXT,
-  created_by_id UUID NOT NULL REFERENCES users (id),
-  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
-  updated_at TIMESTAMPTZ DEFAULT now() NOT NULL
+    id UUID PRIMARY KEY,
+    session_id UUID NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
+    payer_id UUID NOT NULL REFERENCES users (id),
+    amount NUMERIC(12, 2) NOT NULL CHECK (amount >= 0),
+    description TEXT,
+    created_by_id UUID NOT NULL REFERENCES users (id),
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT now() NOT NULL
 );
+
+
+CREATE TRIGGER update_transactions_updated_at BEFORE
+UPDATE ON transactions FOR EACH ROW
+EXECUTE PROCEDURE update_modified_column ();
 
 
 CREATE INDEX idx_transactions_session_id ON transactions (session_id);
@@ -58,9 +81,9 @@ CREATE INDEX idx_transactions_payer_id ON transactions (payer_id);
 
 
 CREATE TABLE transaction_participants (
-  transaction_id UUID NOT NULL REFERENCES transactions (id) ON DELETE CASCADE,
-  user_id UUID NOT NULL REFERENCES users (id),
-  PRIMARY KEY (transaction_id, user_id)
+    transaction_id UUID NOT NULL REFERENCES transactions (id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users (id),
+    PRIMARY KEY (transaction_id, user_id)
 );
 
 
